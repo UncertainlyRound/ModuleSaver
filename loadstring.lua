664a5b1f3f9e5a613a3b6f839f4b0656ccb08b27
UncertainlyRound <20serial@gmail.com>
2025-04-06 09:14:57 +1200

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,6 @@
+-- Settings --
+_G.ModuleToBeSaved =
+_G.SaveAsFile = true
+_G.CopyToClipboard = false
+-- Loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UncertainlyRound/ModuleSaver/refs/heads/main/main.lua"))()
